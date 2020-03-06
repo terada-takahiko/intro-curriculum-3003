b85686a6c17462252d90fc70efdef92e7518da1a
terada takahiko <t.terada@sungroup.co.jp>
2020-03-06 19:15:21 +0900

    人が減ったランキングに改変し、ランキングにしました

--- app.js
+++ app.js
@@ -1,17 +1,41 @@
 'use strict';
-const memo = new Map();
-memo.set(0, 0);
-memo.set(1, 0);
-memo.set(2, 1);
-function trib(n) {
-    if (memo.has(n)) {
-        return memo.get(n);
+const fs = require('fs');
+const readline = require('readline');
+const rs = fs.createReadStream('./popu-pref.csv');
+const rl = readline.createInterface({ 'input': rs, 'output': {} });
+const prefectureDataMap = new Map(); // key: 都道府県 value: 集計データのオブジェクト
+rl.on('line', (linestring) => {
+    const colums = linestring.split(',');
+    const year = parseInt(colums[0]);
+    const prefecture = colums[1];
+    const popu = parseInt(colums[3]);
+    if (year === 2010 || year === 2015) {
+        let value = prefectureDataMap.get(prefecture);
+        if (!value) {
+            value = {
+                popu10: 0,
+                popu15: 0,
+                change: null
+            };
+        }
+        if (year === 2010) {
+            value.popu10 = popu;
+        }
+        if (year === 2015) {
+            value.popu15 = popu;
+        }
+        prefectureDataMap.set(prefecture, value);
     }
-    const value = trib(n - 1) + trib(n - 2) + trib(n - 3);
-    memo.set(n, value);
-    return value;
-}
-const length = 40;
-for (let i = 0; i <= length; i++) {
-    console.log(trib(i));
-}
+});
+rl.on('close', () => {
+    for (let [key, value] of prefectureDataMap) {
+        value.change = value.popu15 / value.popu10;
+    }
+    const rankingArray = Array.from(prefectureDataMap).sort((pair1, pair2) => {
+        return pair1[1].change - pair2[1].change ;   
+    });
+    const rankingStrings = rankingArray.map(([key, value], i) => {
+        return (i + 1) + ': ' + key + ': ' + value.popu10 + '=>' + value.popu15 + ' 変化率:' + value.change;
+      });
+    console.log(rankingStrings);
+});
